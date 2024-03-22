6abcb88537482063ef4c49234a5f3168175b93cf
antisedative <antisedativ@mail.ru>
2024-03-22 03:11:21 +0300

    added utils

---
+++ lib/utils/capitalize_first_letter.dart
@@ -0,0 +1,4 @@
+String capitalizeFirstLetter(String string) {
+  return string.substring(0, 1).toUpperCase() +
+      string.substring(1).toLowerCase();
+}
---
+++ lib/utils/pokemon_stat.dart
@@ -0,0 +1,8 @@
+final Map<String, String> statNameMapping = {
+  'hp': 'HP',
+  'attack': 'Attack',
+  'defense': 'Defense',
+  'special-attack': 'Special Attack',
+  'special-defense': 'Special Defense',
+  'speed': 'Speed',
+};
