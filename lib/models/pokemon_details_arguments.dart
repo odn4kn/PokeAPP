08d99be1cb3675a375bc25e8c814819d5ed15441
antisedative <antisedativ@mail.ru>
2024-03-22 03:09:30 +0300

    unnecessary models removed

--- lib/models/pokemon_details_arguments.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app/models/pokemon_stat.dart';
-
-class PokemonDetailsArguments {
-  final String pokemonName;
-  final PokemonStat pokemonStat;
-
-  PokemonDetailsArguments({required this.pokemonName, required this.pokemonStat});
-}
